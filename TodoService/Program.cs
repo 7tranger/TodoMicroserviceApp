b98ca59547d50d3fd976889efe6c49e0837fb54f
Nikita <nikitaf4488@gmail.com>
2025-07-13 12:39:07 +0300

    Add Swagger

--- TodoService/Program.cs
+++ TodoService/Program.cs
@@ -1,12 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TodoService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services
+  .AddDbContext<TodoContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("TodoDb")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
